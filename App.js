1d1bb39e4b70ffe923cd4c1884d6bfc22da69aa4
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-05-24 15:33:57 -0400

    first commit

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
+      <Button title='Press me please' />
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 38
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ src/components/Counter.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from 'react-native';
+
+export default function Counter() {
+    return(
+        <View>
+            <Text>You clicked the button 0 times.</Text>
+            <Button title="Click Me" />
+        </View>
+    )
+}
